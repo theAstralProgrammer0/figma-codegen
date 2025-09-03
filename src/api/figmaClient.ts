31c6b271364bbe57d37e7b6714681c6cd31a0231
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-09-03 18:32:31 +0100

    Added the api for the figmaClient.ts

---
+++ src/api/figmaClient.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const BASE_URL = "https://api.figma.com/v1";
+
+export async function getFile(fileKey: string): Promise<any> {
+  const token = process.env.FIGMA_API_KEY;
+
+  if (!token) {
+    throw new Error("❌ Missing FIGMA_API_KEY in .env");
+  }
+
+  const response = await axios.get(`${BASE_URL}/files/${fileKey}`, {
+    headers: { "X-Figma-Token": token }
+  });
+
+  return response.data;
+}
+
