352dfa5600bf387f643035b055190578bac5e500
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-09-03 12:26:45 +0100

    Added jest config ts file

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/tests"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  }
+};
+
