3b689963bec3ab88f4fb4a936277600890880a57
theAstralProgrammer0 <tdaisioso@gmail.com>
2025-11-05 05:42:31 +0100

    Exported all figma types in orchestrator index file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,5 @@
-
+export * from './figma/color.interface';
+export * from './figma/effect.interface';
+export * from './figma/grid.interface';
+export * from './figma/paint.interface';
+export * from './figma/node.interface';
